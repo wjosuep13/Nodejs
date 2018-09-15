47f80722c9084059319708f3e2758d535e9f10ac
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2018-09-15 04:19:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ var base64 = require('base-64');
 function decode_base64(base64str, filename) {
   let buf = Buffer.from(base64str, base64);
 
- fs.writeFile(path.join(__dirname, '/public/', filename), buf, function(error) {
+ //fs.writeFile(path.join(__dirname, '/public/', filename), buf, function(error) {
    if (error) {
      throw error;
     } else {
      console.log('File created from base64 string!');
       return true;
    }
- });
+// });
 }
 
 
